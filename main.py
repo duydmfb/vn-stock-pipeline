f3747972acdb1459d81fce129958635a0089819f
Tran_duy <duydmfb@gmail.com>
2026-09-03 10:59:57 +0700

    feat: luu du lieu tho theo ngay dang parquet

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 from vnstock import Quote
 from datetime import datetime
+from pathlib import Path
+
 
 SYMBOL = 'VNM'
-SOURCE = 'VCI'
-START_DATE = '2025-01-01'
-END_DATE = datetime.now().strftime('%Y-%m-%d')
+SOURCE = 'kbs'
+START_DATE = '2026-01-01'
+END_DATE = datetime.now().strftime("%Y-%m-%d") #Ngày cuối lấy dữ liệu
+RAW_DIR = Path("data/raw")
+RUN_DATE = datetime.now().strftime("%Y-%m-%d") #Ngày chạy 
 
 def get_data():
 
     q = Quote(symbol=SYMBOL, source=SOURCE)
     df = q.history(start=START_DATE, end=END_DATE, interval='1D')
-    #df = df[(df["time"] >= START_DATE) & (df["time"] <= END_DATE)]
-    
+
     return df
 
+
+def save_raw(df, symbol):
+    RAW_DIR.mkdir(parents=True, exist_ok=True)
+    ten = f'{RUN_DATE}_{symbol}.parquet'
+    duong_dan = RAW_DIR/ten
+    df.to_parquet(duong_dan, index=False)
+
+    return duong_dan
+
 if __name__ == "__main__":
-    test = get_data()    
-    print(test.shape)
-    print(test.columns.tolist())
-    print(test.head())
-    print(test["time"].min(), test["time"].max())
+    price_df = get_data()    
+    print(price_df.shape)
+    print(price_df.columns.tolist())
+    print(price_df.head())
+    print(price_df.tail(10))
+    print(price_df["time"].min(), price_df["time"].max())
+    raw = save_raw(price_df, SYMBOL)
+    print(f'Du lieu raw đa luu trong {raw}, voi {len(price_df)} dong')
+
